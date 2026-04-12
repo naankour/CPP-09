df2b08039177864884450c7d21a9361eabe85830
Naziha <aankour.naziha@gmail.com>
2026-04-12 23:41:00 +0200

    test a faire

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   PmergeMe.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: naankour <naankour@student.42.fr>          +#+  +:+       +#+        */
+/*   By: naziha <naziha@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/08 14:07:41 by naankour          #+#    #+#             */
-/*   Updated: 2026/04/09 18:33:07 by naankour         ###   ########.fr       */
+/*   Updated: 2026/04/12 23:40:28 by naziha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,91 +83,224 @@ void PmergeMe::run()
 	this->algoVector();
 }
 
-
-std::vector<int> mergeInsertSort(std::vector<int> bigs )
-{
-	std::vector<int> bigPairs;
-	std::vector<int> smallPairs;
-
-
-	int left = -1;
-	bool hasLeft = false;
-
-	if (bigs.size() <= 1)
-	return (bigs);
-	
-	if ((bigs.size() % 2) != 0)
-	{
-		left = bigs.back();
-		bigs.pop_back();
-		hasLeft = true;
-	}
-
-	for (size_t i = 0; i + 1 < bigs.size(); i+=2)
-	{
-		if (bigs[i] > bigs[i + 1])
-		{
-			bigPairs.push_back(bigs[i]);
-			smallPairs.push_back(bigs[i+1]);
-		}
-		else
-		{
-			bigPairs.push_back(bigs[i+1]);
-			smallPairs.push_back(bigs[i]);
-		}	
-	}
-	bigPairs = mergeInsertSort(bigPairs);
-	
-	std::vector<int> sorted;
-	sorted.push_back(smallPairs[0]);
-	for(size_t i = 0; i < bigPairs.size(); i++)
-		sorted.push_back(bigPairs[i]);
-
-	if (hasLeft)
-	{
-		std::vector<int>::iterator position = std::lower_bound(sorted.begin(), sorted.end(), left);
-		sorted.insert(position, left);
-	}
-	
-	std::cout << "SORTED: ";
-	for (size_t i = 0; i < sorted.size(); i++)
-		std::cout << sorted[i] << " ";
-	std::cout << std::endl;
-
-	return(sorted);
-}
-
-
 std::vector<size_t> buildJacob(size_t size)
 {
-    std::vector<size_t> order;
-    std::vector<size_t> jacob;
-
+	std::vector<size_t> order;
+	
     if (size == 0)
-        return (order);
-
+		return (order);
+	
+	if (size == 1)
+	{
+		order.push_back(0);
+		return (order);
+	}
+	
+	std::vector<size_t> jacob;
+    jacob.push_back(0);
     jacob.push_back(1);
-    jacob.push_back(3);
 
     while (jacob.back() < size)
     {
         size_t next = jacob[jacob.size() - 1] + 2 * jacob[jacob.size() - 2];
         jacob.push_back(next);
     }
+	std::cout << "JACOB SEQ: ";
+	for (size_t i = 0; i < jacob.size(); i++)
+		std::cout << jacob[i] << " ";
+	std::cout << std::endl;
+	
+	std::vector<bool> used(size, false);
 
-    size_t prev = 1;
     for (size_t i = 1; i < jacob.size(); i++)
     {
-        size_t curr = jacob[i];
-        for (size_t j = curr; j > prev; j--)
-        {
-            if (j <= size)
-                order.push_back(j);
-        }
-        prev = curr;
-    }
+        size_t start = jacob[i - 1] + 1;
+		size_t end;
+
+        if (jacob[i] < size)
+            end = jacob[i];
+		else
+			end = size - 1;
+	
+		for (size_t j = end; j >= start && j < size; j--)
+		{
+			if (used[j] == false)
+			{
+				order.push_back(j);
+				used[j] = true;
+			}
+		}
+	}
+
+	for (size_t i = 0; i < size; i++)
+	{
+		if (used[i] == false)
+			order.push_back(i);
+	}
+	std::cout << "ORDER INSERTION: ";
+	for (size_t i = 0; i < order.size(); i++)
+		std::cout << order[i] << " ";
+	std::cout << std::endl;
     return (order);
 }
+
+
+
+// std::vector<int> mergeInsertSort(std::vector<int> arr)
+// {
+//     std::vector<int> bigPairs;
+//     std::vector<int> smallPairs;
+
+//     int straggler = -1;
+//     bool hasStraggler = false;
+
+//     if (arr.size() <= 1)
+//         return (arr);
+    
+//     // ✅ Gérer l'impair
+//     if ((arr.size() % 2) != 0)
+//     {
+//         straggler = arr.back();
+//         arr.pop_back();
+//         hasStraggler = true;
+//     }
+
+//     // ✅ ÉTAPE 1 : Former les paires 2 par 2
+//     for (size_t i = 0; i + 1 < arr.size(); i += 2)
+//     {
+//         if (arr[i] > arr[i + 1])
+//         {
+//             bigPairs.push_back(arr[i]);
+//             smallPairs.push_back(arr[i + 1]);
+//         }
+//         else
+//         {
+//             bigPairs.push_back(arr[i + 1]);
+//             smallPairs.push_back(arr[i]);
+//         }	
+//     }
+
+//     std::cout << "Paires formées:" << std::endl;
+//     for (size_t i = 0; i < bigPairs.size(); i++)
+//     {
+//         std::cout << "  big: " << bigPairs[i] << " small: " << smallPairs[i] << std::endl;
+//     }
+//     if (hasStraggler)
+//         std::cout << "  Straggler: " << straggler << std::endl;
+    
+//     // ✅ ÉTAPE 2 : RÉCURSION sur les bigs pour refaire des paires de paires
+//     bigPairs = mergeInsertSort(bigPairs);
+    
+//     // ✅ ÉTAPE 3 : Créer result avec les bigs triés
+//     std::vector<int> result = bigPairs;
+
+//     // ✅ ÉTAPE 4 : Insérer les smalls avec Jacobsthal
+//     std::vector<size_t> jacob = buildJacob(smallPairs.size());
+//     for (size_t i = 0; i < jacob.size(); i++)
+//     {
+//         size_t index = jacob[i];
+//         if (index < smallPairs.size())
+//         {
+//             std::vector<int>::iterator position = std::lower_bound(result.begin(), result.end(), smallPairs[index]);
+//             result.insert(position, smallPairs[index]);
+//         }
+//     }
+    
+//     // ✅ ÉTAPE 6 : Insérer le straggler
+//     if (hasStraggler)
+//     {
+//         std::vector<int>::iterator position = std::lower_bound(result.begin(), result.end(), straggler);
+//         result.insert(position, straggler);
+//     }
+
+//     return (result);
+// }
+
+
+// void PmergeMe::algoVector()
+// {
+//     std::cout << "Before: ";
+//     for (size_t i = 0; i < this->vector.size(); i++)
+//         std::cout << this->vector[i] << " ";
+//     std::cout << std::endl;
+
+//     // ✅ APPEL UNIQUE avec TOUTE la liste
+//     this->vector = mergeInsertSort(this->vector);
+
+//     std::cout << "After: ";
+//     for (size_t i = 0; i < this->vector.size(); i++)
+//         std::cout << this->vector[i] << " ";
+//     std::cout << std::endl;
+// }
+std::vector<int> mergeInsertSort(std::vector<int> arr )
+{
+	std::vector<int> bigPairs;
+	std::vector<int> smallPairs;
+
+	int left = -1;
+	bool hasLeft = false;
+
+	if (arr.size() <= 1)
+		return (arr);
+	
+	if ((arr.size() % 2) != 0)
+	{
+		left = arr.back();
+		arr.pop_back();
+		hasLeft = true;
+	}
+
+	for (size_t i = 0; i + 1 < arr.size(); i+=2)
+	{
+		if (arr[i] > arr[i + 1])
+		{
+			bigPairs.push_back(arr[i]);
+			smallPairs.push_back(arr[i+1]);
+		}
+		else
+		{
+			bigPairs.push_back(arr[i+1]);
+			smallPairs.push_back(arr[i]);
+		}	
+	}
+	
+	bigPairs = mergeInsertSort(bigPairs);
+	
+	std::vector<int> result = bigPairs;
+
+	std::vector<size_t> jacob = buildJacob(smallPairs.size());
+	for (size_t i = 0; i < jacob.size(); i++)
+    {
+        size_t index = jacob[i];
+        if (index < smallPairs.size())
+        {
+            std::vector<int>::iterator position = std::lower_bound(result.begin(), result.end(), smallPairs[index]);
+            result.insert(position, smallPairs[index]);
+        }
+    }
+
+	for (int i = bigPairs.size() - 1; i >= 1; i--)
+    {
+        std::vector<int>::iterator position = std::lower_bound(result.begin(), result.end(), bigPairs[i]);
+        result.insert(position, bigPairs[i]);
+    }
+	
+	if (hasLeft)
+	{
+		std::vector<int>::iterator position = std::lower_bound(result.begin(), result.end(), left);
+		result.insert(position, left);
+	}
+	
+	std::cout << "SORTED: ";
+	for (size_t i = 0; i < result.size(); i++)
+		std::cout << result[i] << " ";
+	std::cout << std::endl;
+
+	return(result);
+}
+
+
+
 
 void PmergeMe::algoVector()
 {
@@ -221,44 +354,23 @@ void PmergeMe::algoVector()
 
 	std::vector<int> finalChain;
 	finalChain = mergeInsertSort(bigs);
-	finalChain.insert(finalChain.begin(), smalls[0]);
 
-	std::cout << "SORTED with smallest: ";
-	for (size_t i = 0; i < finalChain.size(); i++)
-		std::cout << finalChain[i] << " ";
-	std::cout << std::endl;
-
-	std::vector<size_t> jacob = buildJacob(smalls.size());
+	std::vector<size_t> jacobOrder = buildJacob(smalls.size());
 	std::cout << "JACOB: ";
-	for (size_t i = 0; i < jacob.size(); i++)
-		std::cout << jacob[i] << " ";
+	for (size_t i = 0; i < jacobOrder.size(); i++)
+		std::cout << jacobOrder[i] << " ";
 	std::cout << std::endl;
-	
-	std::vector<bool> inserted(smalls.size(), false);
 
-	for(size_t i = 0; i < jacob.size(); i++)
+	for(size_t i = 0; i < jacobOrder.size(); i++)
 	{
-		size_t index = jacob[i] - 1;
-		if (index >= smalls.size())
-			continue;
-		std::vector<int>::iterator position = std::lower_bound(finalChain.begin(), finalChain.end(), smalls[index]);
-		finalChain.insert(position, smalls[index]);
-		inserted[index] = true;
+		size_t index = jacobOrder[i];
+		if (index < smalls.size())
+		{
+			std::vector<int>::iterator position = std::lower_bound(finalChain.begin(), finalChain.end(), smalls[index]);
+			finalChain.insert(position, smalls[index]);
+		}
+		
 	}
-
-	for(size_t i = 0; i < smalls.size(); i++)
-	{
-		if (inserted[i] == true)
-			continue;
-
-		std::vector<int>::iterator position = std::lower_bound(finalChain.begin(), finalChain.end(), smalls[i]);
-		finalChain.insert(position, smalls[i]);
-	}
-
-	std::cout << "After: ";
-	for (size_t i = 0; i < finalChain.size(); i++)
-		std::cout << finalChain[i] << " ";
-	std::cout << std::endl;
 
 	if (left != -1)
 	{
@@ -266,6 +378,11 @@ void PmergeMe::algoVector()
 		finalChain.insert(position, left);
 	}
 	
+	std::cout << "After: ";
+	for (size_t i = 0; i < finalChain.size(); i++)
+		std::cout << finalChain[i] << " ";
+	std::cout << std::endl;
+
 	// finalChain.push_back(smallPairs[0].big);
 	// for (size_t i = 0; i < bigPairs.size(); i++)
 	// 	finalChain.push_back(bigPairs[i].big);
